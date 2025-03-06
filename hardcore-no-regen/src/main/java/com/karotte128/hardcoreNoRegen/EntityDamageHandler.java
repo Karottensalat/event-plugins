2814a2b174b4a19edf602c9836a3039216eae2a1
Karotte128 <Karotte128@outlook.de>
2025-03-06 16:15:29 +0100

    fix shields not working

--- hardcore-no-regen/src/main/java/com/karotte128/hardcoreNoRegen/EntityDamageHandler.java
+++ hardcore-no-regen/src/main/java/com/karotte128/hardcoreNoRegen/EntityDamageHandler.java
@@ -19,11 +19,13 @@ public class EntityDamageHandler implements Listener {
                 event.setCancelled(true);
             } else {
                 Player player = (Player) entity;
-                double remainingHealth = player.getHealth() - event.getDamage();
+                double remainingHealth = player.getHealth() - event.getFinalDamage();
                 player.getAttribute(Attribute.MAX_HEALTH).setBaseValue(remainingHealth);
 
-                Component damageMessage = Component.text("Player " + player.getName() + " took " + event.getDamage() + "HP damage, only " + remainingHealth + "HP remaining!");
-                HardcoreNoRegen.getInstance().getServer().broadcast(damageMessage);
+                if (event.getFinalDamage() > 0) {
+                    Component damageMessage = Component.text("Player " + player.getName() + " took " + event.getFinalDamage() + "HP damage, only " + remainingHealth + "HP remaining!");
+                    HardcoreNoRegen.getInstance().getServer().broadcast(damageMessage);
+                }
             }
         }
     }
