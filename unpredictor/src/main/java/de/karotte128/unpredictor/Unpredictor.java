8676eebfadb1302f521457ff641ae974a17fe226
Karotte128 <Karotte128@outlook.de>
2025-04-01 18:17:20 +0200

    detect new day

--- unpredictor/src/main/java/de/karotte128/unpredictor/Unpredictor.java
+++ unpredictor/src/main/java/de/karotte128/unpredictor/Unpredictor.java
@@ -1,17 +1,34 @@
 package de.karotte128.unpredictor;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
 public final class Unpredictor extends JavaPlugin {
+    private static Unpredictor instance;
+    private static int taskId;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
 
     @Override
     public void onEnable() {
+        BukkitScheduler scheduler = getServer().getScheduler();
+        taskId = scheduler.scheduleSyncRepeatingTask(this, new NewDayDetector(), 0, 1);
+
         getCommand("unpredictor").setExecutor(new UnpredictorCommand());
 
+        getLogger().info("Unpredictor has been loaded!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getServer().getScheduler().cancelTask(taskId);
+        getLogger().info("Unpredictor has been unloaded!");
+    }
+
+    public static Unpredictor getInstance() {
+        return instance;
     }
 }
