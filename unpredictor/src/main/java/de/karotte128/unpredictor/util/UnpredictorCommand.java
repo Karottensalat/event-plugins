3b21f73e2da858d3e6cec490748410e5c5f4f106
Karotte128 <Karotte128@outlook.de>
2025-04-02 18:43:30 +0200

    command completer

--- unpredictor/src/main/java/de/karotte128/unpredictor/util/UnpredictorCommand.java
+++ unpredictor/src/main/java/de/karotte128/unpredictor/util/UnpredictorCommand.java
@@ -1,12 +1,16 @@
 package de.karotte128.unpredictor.util;
 
+import de.karotte128.unpredictor.challenges.ChallengeManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UnpredictorCommand implements CommandExecutor, TabExecutor {
@@ -19,6 +23,20 @@ public class UnpredictorCommand implements CommandExecutor, TabExecutor {
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        return List.of();
+        final List<String> validArguments = new ArrayList<>();
+
+        if (strings.length == 1) {
+            StringUtil.copyPartialMatches(strings[0], List.of("setchallenge"), validArguments);
+            return validArguments;
+        }
+
+        if (strings.length == 2) {
+            switch (strings[0]) {
+                case "setchallenge":
+                    StringUtil.copyPartialMatches(strings[1], Arrays.stream(ChallengeManager.getAllChallenges()).toList(), validArguments);
+            }
+        }
+
+        return validArguments;
     }
 }
