47ede3a63977aac7968b2c3da5e094fb1b63a47c
Karotte128 <Karotte128@outlook.de>
2025-04-14 17:39:41 +0200

    fix Kevlorts mistakes

--- unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/DamagingWaterChallenge.java
+++ unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/DamagingWaterChallenge.java
@@ -2,37 +2,29 @@ package de.karotte128.unpredictor.challenge.challenges;
 
 import de.karotte128.unpredictor.challenge.DefaultChallenge;
 import net.kyori.adventure.text.Component;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
-public class TestChallenge extends DefaultChallenge {
-
-    private int inWaterTask;
-    private final BukkitScheduler scheduler = server.getScheduler();
+public class DamagingWaterChallenge extends DefaultChallenge {
 
     @Override
     public void load() {
-        BukkitScheduler scheduler = server.getScheduler();
-        inWaterTask = scheduler.scheduleSyncRepeatingTask(Unpredictor.getInstance(), runTask(), 0, 20);
-        server.broadcast(Component.text("load test challenge"));
+        scheduleTask(20);
+        server.broadcast(Component.text("load water damage challenge"));
     }
 
     @Override
     public void unload() {
-        server.broadcast(Component.text("unload test challenge"));
-        scheduler.cancelTask(inWaterTask);
+        server.broadcast(Component.text("unload water damage challenge"));
     }
 
-    private @NotNull Runnable runTask() {
-        return () -> {
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                Material blockTyp = player.getLocation().getBlock().getType();
-                if(blockTyp == Material.WATER) {
-                    player.damage(2);
-                }
+    public void runTask() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            Material blockTyp = player.getLocation().getBlock().getType();
+            if (blockTyp == Material.WATER) {
+                player.damage(2);
             }
-        };
+        }
     }
 }
