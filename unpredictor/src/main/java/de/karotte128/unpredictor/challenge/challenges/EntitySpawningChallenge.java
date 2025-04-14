a897cc493fc3e4d1c3b20bff5e1eb8d5c966cb37
Kevlort <143482489+Kevlort@users.noreply.github.com>
2025-04-15 01:07:13 +0200

    Entity spawnig updat4

--- unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/EntitySpawningChallenge.java
+++ unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/EntitySpawningChallenge.java
@@ -2,29 +2,33 @@ package de.karotte128.unpredictor.challenge.challenges;
 
 import de.karotte128.unpredictor.challenge.DefaultChallenge;
 import de.karotte128.unpredictor.util.Debug;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.minecart.SpawnerMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntitySpawnEvent;
-import java.util.HashMap;
+
+import java.util.*;
 
 public class EntitySpawningChallenge extends DefaultChallenge {
 
+    private ArrayList<EntityType> preList = new ArrayList<>();
     private HashMap<EntityType,EntityType> entityTypeHashMap = new HashMap<>();
 
     @Override
     public void load() {
-        for (EntityType type : EntityType.values()) {
-            if (type != EntityType.PLAYER) {
-                if (entityTypeHashMap.containsKey(null)) {
-                    entityTypeHashMap.put(type, entityTypeHashMap.get(null));
-                    entityTypeHashMap.put(entityTypeHashMap.get(null), type);
-                } else {
-                    entityTypeHashMap.put(null, type);
-                }
+        for (EntityType t :EntityType.values()){
+            if (t != EntityType.PLAYER || t != EntityType.ITEM || t != EntityType.UNKNOWN) {
+                preList.add(t);
+            }
+        }
+        Collections.shuffle(preList);
+        for (EntityType type : preList) {
+            if (entityTypeHashMap.containsKey(null)) {
+                entityTypeHashMap.put(type, entityTypeHashMap.get(null));
+                entityTypeHashMap.put(entityTypeHashMap.get(null), type);
+            } else {
+                entityTypeHashMap.put(null, type);
             }
         }
         Debug.debugMessage("load entity spawn challenge");
@@ -47,7 +51,12 @@ public class EntitySpawningChallenge extends DefaultChallenge {
         Entity entity = event.getEntity();
         EntityType entityType = event.getEntityType();
         EntityType newType = entityTypeHashMap.get(entityType);
-        entity.getWorld().spawnEntity(entity.getLocation(),newType);
+//        entity.getWorld().spawnEntity(entity.getLocation(),newType);
+        SpawnerMinecart w = (SpawnerMinecart) entity.getWorld().spawnEntity(entity.getLocation(),EntityType.SPAWNER_MINECART);
+        w.spawnAt(entity.getLocation());
+        w.setSpawnedType(newType);
+        w.setMaxSpawnDelay(0);
+        w.remove();
         entity.remove();
     }
 }
