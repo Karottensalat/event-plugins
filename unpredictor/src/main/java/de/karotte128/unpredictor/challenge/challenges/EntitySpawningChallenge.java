f1315c4e4cc4409b7ff9c9a56c934b1b1ecc0aed
Kevlort <143482489+Kevlort@users.noreply.github.com>
2025-04-15 15:29:45 +0200

    Entity spawnig updat14 unter wasser Animals werden raus gerechnet

--- unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/EntitySpawningChallenge.java
+++ unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/EntitySpawningChallenge.java
@@ -3,9 +3,11 @@ package de.karotte128.unpredictor.challenge.challenges;
 import de.karotte128.unpredictor.Unpredictor;
 import de.karotte128.unpredictor.challenge.DefaultChallenge;
 import de.karotte128.unpredictor.util.Debug;
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.SpawnCategory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -17,16 +19,25 @@ public class EntitySpawningChallenge extends DefaultChallenge {
 
     private HashMap<EntityType,EntityType> entityTypeHashMap = new HashMap<>();
     private HashMap<EntityType,Integer> spawnLimitHashMap = new HashMap<>();
+    private HashMap<SpawnCategory,Integer> orgnialSpawnLimitHashMap = new HashMap<>();
 
     @Override
     public void load() {
         Debug.debugMessage("load entity spawn challenge");
+        for (SpawnCategory spawnCategory : SpawnCategory.values()) {
+            orgnialSpawnLimitHashMap.put(spawnCategory,Bukkit.getSpawnLimit(spawnCategory));
+        }
     }
 
     @Override
     public void unload() {
         entityTypeHashMap = null;
         spawnLimitHashMap = null;
+        for (World world1 :Bukkit.getWorlds()) {
+            for (SpawnCategory spawnCategory : SpawnCategory.values()) {
+                world1.setSpawnLimit(spawnCategory, orgnialSpawnLimitHashMap.get(spawnCategory));
+            }
+        }
         Debug.debugMessage("unload entity spawn challenge");
     }
 
@@ -44,7 +55,7 @@ public class EntitySpawningChallenge extends DefaultChallenge {
             if (entityTypeHashMap.get(entityType) == null) {
                 if (entityType != EntityType.PLAYER && entityType != EntityType.ITEM && entityType != EntityType.UNKNOWN) {
                     entityTypeHashMap.put(entityType, getRandomEntity());
-                    spawnLimitHashMap.put(entityType, world.getSpawnLimit(entity.getSpawnCategory()));
+                    spawnLimitHashMap.put(entityType, entity.getWorld().getSpawnLimit(entity.getSpawnCategory()));
                 }
             }
 
