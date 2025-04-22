6b9c5ef230fe60fe2bfb9558bff097141d876b61
Karotte128 <karotte128@outlook.de>
2025-04-22 19:48:07 +0200

    spawn zombies

--- unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/AttackingBlocksChallenge.java
+++ unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/AttackingBlocksChallenge.java
@@ -4,9 +4,14 @@ import de.karotte128.unpredictor.challenge.DefaultChallenge;
 import de.karotte128.unpredictor.util.Debug;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.BlockDisplay;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.util.Transformation;
+import org.joml.AxisAngle4f;
+import org.joml.Vector3f;
 
 public class AttackingBlocksChallenge extends DefaultChallenge {
 
@@ -19,7 +24,7 @@ public class AttackingBlocksChallenge extends DefaultChallenge {
     public void unload() {
         server.getWorlds().forEach(world -> {
             world.getEntities().forEach(entity -> {
-                if (entity.getScoreboardTags().contains("unpredictor_block")) {
+                if (entity.getScoreboardTags().contains("unpredictor_attacking_block")) {
                     entity.remove();
                 }
             });
@@ -36,9 +41,21 @@ public class AttackingBlocksChallenge extends DefaultChallenge {
     public void onBlockBreak(BlockBreakEvent event) {
         Material blockMaterial = event.getBlock().getType();
         Location blockLocation = event.getBlock().getLocation();
+        World world = event.getBlock().getWorld();
+
+        Transformation transformation = new Transformation(new Vector3f(0, -1, 0), new AxisAngle4f(), new Vector3f(1, 1, 1), new AxisAngle4f());
 
         BlockDisplay blockDisplay = event.getBlock().getWorld().spawn(blockLocation, BlockDisplay.class);
         blockDisplay.setBlock(blockMaterial.createBlockData());
-        blockDisplay.addScoreboardTag("unpredictor_block");
+        blockDisplay.setTransformation(transformation);
+        blockDisplay.addScoreboardTag("unpredictor_attacking_block");
+
+        Zombie zombie = world.spawn(blockLocation, Zombie.class);
+        zombie.setInvisible(true);
+        zombie.setShouldBurnInDay(false);
+        zombie.setBaby();
+        zombie.addScoreboardTag("unpredictor_attacking_block");
+
+        zombie.addPassenger(blockDisplay);
     }
 }
