9d99f6223c351ac46b1d0e511d0c46610d74eaee
Karotte128 <karotte128@outlook.de>
2025-04-23 18:37:12 +0200

    bug fixes

--- unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/LightningSpawnsEntitiesChallenge.java
+++ unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/LightningSpawnsEntitiesChallenge.java
@@ -4,6 +4,8 @@ import de.karotte128.unpredictor.Unpredictor;
 import de.karotte128.unpredictor.challenge.DefaultChallenge;
 import de.karotte128.unpredictor.util.Debug;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.weather.LightningStrikeEvent;
@@ -12,15 +14,27 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class LightningSpawnsEntitiesChallenge extends DefaultChallenge {
+    private static final World world = Unpredictor.getInstance().getServer().getWorld("world");
+
     @Override
     public void load() {
-        Unpredictor.getInstance().getServer().getWorld("world").setThundering(true);
-        Unpredictor.getInstance().getServer().getWorld("world").setThunderDuration(24000);
+        world.setThundering(true);
+        world.setThunderDuration(24000);
         Debug.debugMessage("load lightning spawning challenge");
     }
 
     @Override
     public void unload() {
+        world.setThundering(false);
+
+        server.getWorlds().forEach(world -> {
+            world.getEntities().forEach(entity -> {
+                if (entity.getScoreboardTags().contains("unpredictor_lightning_entity")) {
+                    entity.remove();
+                }
+            });
+        });
+
         Debug.debugMessage("unload lightning spawning challenge");
     }
 
@@ -34,7 +48,8 @@ public class LightningSpawnsEntitiesChallenge extends DefaultChallenge {
         Location location = event.getLightning().getLocation();
         EntityType type = getRandomEntity();
         if (type != EntityType.PLAYER && type != EntityType.ITEM && type != EntityType.UNKNOWN) {
-            event.getWorld().spawnEntity(location, type);
+            Entity entity = event.getWorld().spawnEntity(location, type);
+            entity.addScoreboardTag("unpredictor_lightning_entity");
         }
     }
 
