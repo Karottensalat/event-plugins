eed0ed8a861baf6e1deea7eab857dc6b294555c1
Karotte128 <karotte128@outlook.de>
2025-04-24 15:31:47 +0200

    rename challenge

--- unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/SneakClearInvChallenge.java
+++ unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/SneakClearInvChallenge.java
@@ -5,21 +5,21 @@ import de.karotte128.unpredictor.util.Debug;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
-public class ClearSneakChallenge extends DefaultChallenge {
+public class SneakClearInvChallenge extends DefaultChallenge {
 
     @Override
     public void load() {
-        Debug.debugMessage("load test challenge");
+        Debug.debugMessage("load sneak clear inv challenge");
     }
 
     @Override
     public void unload() {
-        Debug.debugMessage("unload test challenge");
+        Debug.debugMessage("unload sneak clear inv challenge");
     }
 
     @Override
     public void runTask() {
-
+        //nothing here
     }
 
     @EventHandler
