9b4355a2a90c5473fff4e13e329b9a7d206f7094
Karotte128 <karotte128@outlook.de>
2025-04-24 19:47:47 +0200

    zombie apocalypse challenge

---
+++ unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/ZombieApocalypseChallenge.java
@@ -0,0 +1,42 @@
+package de.karotte128.unpredictor.challenge.challenges;
+
+import de.karotte128.unpredictor.challenge.DefaultChallenge;
+import de.karotte128.unpredictor.util.Debug;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+
+public class ZombieApocalypseChallenge extends DefaultChallenge {
+    private final World world = server.getWorld("world");
+
+
+    @Override
+    public void load() {
+        scheduleTask(20);
+        Debug.debugMessage("load zombie apocalypse challenge");
+    }
+
+    @Override
+    public void unload() {
+        world.getEntities().forEach(entity -> {
+            if (entity.getScoreboardTags().contains("unpredictor_zombie_apocalypse")) {
+                entity.remove();
+            }
+        });
+        Debug.debugMessage("unload zombie apocalypse challenge");
+    }
+
+    @Override
+    public void runTask() {
+        world.getPlayers().forEach(this::spawnZombie);
+    }
+
+    private void spawnZombie(Player player) {
+        Location spawnLocation = player.getLocation(); //TODO: add some spread logic
+
+        Zombie zombie = world.spawn(spawnLocation, Zombie.class);
+        zombie.addScoreboardTag("unpredictor_zombie_apocalypse");
+        zombie.setShouldBurnInDay(false);
+    }
+}
