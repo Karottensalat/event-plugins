e8d44ea7ebaa54ed983bed99a4d5eb9d6994fd57
Karotte128 <karotte128@outlook.de>
2025-04-25 20:36:34 +0200

    fix anvil challenge

--- unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/FallingAnvilChallenge.java
+++ unpredictor/src/main/java/de/karotte128/unpredictor/challenge/challenges/FallingAnvilChallenge.java
@@ -2,12 +2,9 @@ package de.karotte128.unpredictor.challenge.challenges;
 
 import de.karotte128.unpredictor.challenge.DefaultChallenge;
 import de.karotte128.unpredictor.util.Debug;
-import net.kyori.adventure.text.Component;
 import org.bukkit.*;
 import org.bukkit.block.*;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.BlockBreakEvent;
 
 import java.util.Random;
 
@@ -15,43 +12,33 @@ public class FallingAnvilChallenge extends DefaultChallenge {
 
     @Override
     public void load() {
-        Debug.debugMessage("load test challenge");
+        Debug.debugMessage("load falling anvil challenge");
         int task = scheduleTask(5);
         Debug.debugMessage("scheduled task " + task);
     }
 
     @Override
     public void unload() {
-        Debug.debugMessage("unload test challenge");
+        Debug.debugMessage("unload falling anvil challenge");
     }
 
     @Override
     public void runTask() {
-        Bukkit.getOnlinePlayers().forEach(player -> spawnAnvil(player));
-        server.broadcast(Component.text("running test challenge task"));
-    }
-
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        server.broadcast(Component.text("Block break"));
+        server.getOnlinePlayers().forEach(this::spawnAnvil);
     }
 
     private void spawnAnvil(Player player){
-        Block anvil = player.getWorld().getBlockAt(spawnLocation(player.getLocation()));
-        anvil.setType(Material.DAMAGED_ANVIL);
+        if (player.getLocation().getWorld() == server.getWorld("world")) {
+            Block anvil = player.getWorld().getBlockAt(spawnLocation(player.getLocation()));
+            anvil.setType(Material.DAMAGED_ANVIL);
+        }
     }
 
-    private Location spawnLocation(Location playerLocation){
-        if (playerLocation.getWorld().getEnvironment().equals(World.Environment.NETHER)) {
-            Random rand = new Random();
-            int grosse = 8;
-            int bereichStartX = playerLocation.getBlockX() - grosse;
-            int bereichStartY = playerLocation.getBlockY() - grosse;
-            int hohe = playerLocation.getWorld().getHighestBlockYAt(bereichStartX + rand.nextInt(16), bereichStartY + rand.nextInt(16));
-            Location location = new Location(playerLocation.getWorld(), bereichStartX + rand.nextInt(16), bereichStartY + rand.nextInt(16), hohe + 30);
-            return location;
-        }else {
-            return null;
-        }
+    private Location spawnLocation(Location playerLocation) {
+        Random rand = new Random();
+        int x = playerLocation.getBlockX() - 8 + rand.nextInt(16);
+        int z = playerLocation.getBlockY() - 8 + rand.nextInt(16);
+        int y = playerLocation.getWorld().getHighestBlockYAt(x , z) + 30;
+        return new Location(playerLocation.getWorld(), x, y, z);
     }
 }
